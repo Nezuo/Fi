9fbb58f7415d4e87f71b12d26f0c5bb7068832fd
Micah Hinckley <hinckleytmicah@gmail.com>
2020-10-18 20:24:41 -0700

    Add auto save interval constant

--- src/Constants.lua
+++ src/Constants.lua
@@ -2,6 +2,8 @@
 return {
     USE_MOCK_DATA_STORE = false; -- When set to true, Fi will use the MockDataStoreService.
 
+    AUTO_SAVE_INTERVAL = 30; -- Time in seconds between auto save for a profile. This value may vary.
+
     LOAD_RETRY_DELAY = 6; -- Time between UpdateAsync attemps when loading a profile.
     TIME_BEFORE_FORCE_STEAL = 60 -- Time in seconds before it force steals a profile. TODO: Figure out how long this should be.
 }
